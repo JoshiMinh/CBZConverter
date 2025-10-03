ce3d6904b01a4496f2465e2e6d9491ad83cb28d8
Joshi Minh <binhangia241273@gmail.com>
2025-10-03 09:24:09 +0700

    final update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.joshiminh.cbzconverter"
         minSdk = 24
 
-        versionCode = 1
-        versionName = "1.0.6"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
